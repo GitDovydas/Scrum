edcdd72e98d2df564ce617ec2cca3faea58feaba
GitDovydas <dovydas.samoska@gmail.com>
2023-03-10 12:03:26 +0200

    Added login GUI with validation

--- src/helpdesk/Main.java
+++ src/helpdesk/Main.java
@@ -13,14 +13,19 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         // Nurodoma kur yra vaizdas (Kur yra grafinė vartotojo sąsaja (Forma į kuria vesime duomenis))
         Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+
+        // Susikuriama scena kuri yra pakonfiguruojama
+        Scene loginScene = new Scene(root, 300, 275);
+        // Scenai nurodomas CSS failiukas pagal kurį bus modifikuojami scenoje esantys elementai
+        loginScene.getStylesheets().add(getClass().getResource("view/CSS.css").toExternalForm());
+
         // Šiuo metu rodomas dialogas, nustatoma jo antraštė
         primaryStage.setTitle("Login");
         // Nustatomas dialogo plotas ir ilgis, ir priskiriama dialogui grafinė vartotojo sąsaja (Forma), tai yra užkraunami GUI
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(loginScene);
         // Reikalinga kad rodytų dialoga
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         // Paleidžiamas metodas, kuris startuos GUI aplikaciją (Start metodas)
