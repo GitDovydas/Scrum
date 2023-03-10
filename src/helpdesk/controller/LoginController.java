6077bab1db5bb4f176055da0c7f114d0a8324820
DomasKITM <domas.babicas@stud.kitm.lt>
2023-03-10 12:03:58 +0200

    Added Login screen

--- src/helpdesk/controller/LoginController.java
+++ src/helpdesk/controller/LoginController.java
@@ -1,4 +1,36 @@
 package helpdesk.controller;
 
+import helpdesk.utils.Validation;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class LoginController {
+    @FXML
+    Button login_button;
+    @FXML
+    TextField username;
+    @FXML
+    PasswordField password;
+    @FXML
+    Label login_error;
+    public void onLoginButtonClick(ActionEvent actionEvent) {
+        // Aprašomas mygtuko paspaudimas
+        login_button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // Aprašomi veiksmai, kurie bus atlikti paspaudus mygtuką
+                // Atliekama vartotojo įvestų duomenų validacija
+                if (Validation.isValidUsername(username.getText()) && Validation.isValidPassword(password.getText())) {
+                    login_error.setText("Username: " + username.getText() + "\n" + "Password: " + password.getText());
+                } else {
+                    login_error.setText("Wrong username or password");
+                }
+            }
+        });
+    }
 }
---
+++ src/helpdesk/utils/Validation.java
@@ -0,0 +1,30 @@
+package helpdesk.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+    // Apsirašomi validacijos šablonai pgal kuruos tikrinsime vartotojo įestus duomenis
+    public static final String USERNAME_REGEX_PATTERN = "^[a-zA-Z0-9]{5,13}$";
+    public static final String PASSWORD_REGEX_PATTERN = "^[a-zA-Z-0-9!@#$%]{5,13}$";
+
+    /**
+     * Funkcija tikrinanati ar varototjo įvesti duoemnys prisijungimo vardui atitinka validacijos šabloną
+     * @param username vartotojo įvestas prisijungimo vardas
+     * @return true - jeigu vartotojo įvestas vardas atitinka šabloną, false - priešingu atveju
+     */
+    public static boolean isValidUsername(String username) {
+        // Pagal 7 eilutes apsirašytą šabloną sukuriamas sukuriamos taisyklės
+        Pattern pattern = Pattern.compile(USERNAME_REGEX_PATTERN);
+        // Vartotojo įvestas prisijungimo vardas palyginamas su aukščiau sukurtom taisyklėm
+        Matcher matcher = pattern.matcher(username);
+        // true - jeigu vartotojo įvestas vardas atitinka šabloną, false - priešingu atveju
+        return matcher.find();
+    }
+
+    public static boolean isValidPassword(String password) {
+        Pattern pattern = Pattern.compile(PASSWORD_REGEX_PATTERN);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.find();
+    }
+}
